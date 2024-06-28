00726b0ee075fd33d719368bb8a396a73b5c8ade
interbattles <88935519+interbattles@users.noreply.github.com>
2024-06-27 17:38:45 -0700

    format with fish indent to pass tests

--- functions/__starship_init.fish
+++ functions/__starship_init.fish
@@ -1,6 +1,6 @@
 function __starship_init
-  set --query starship_cache; and set -fx STARSHIP_CACHE $starship_cache
-  set --query starship_config; and set -fx STARSHIP_CONFIG $starship_config
- 
-  command starship init fish | source
+    set --query starship_cache; and set -fx STARSHIP_CACHE $starship_cache
+    set --query starship_config; and set -fx STARSHIP_CONFIG $starship_config
+
+    command starship init fish | source
 end
